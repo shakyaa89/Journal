7534504538e0049a6205d7dfa61085319e4b8067
shakyaa89 <shashwatshakya89@gmail.com>
2025-12-18 16:43:12 +0545

    Added Home Page, Add Journal Page, Journals List Page and created add journal logic by appending to list

---
+++ Journal/Models/Journal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project.Models   // <-- Make sure this is NOT 'Journal'
+{
+    public class Journal
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        // Static list to hold all journal entries
+        public static List<Journal> Journals { get; } = new List<Journal>();
+
+        // Method to save a journal entry
+        public void Save()
+        {
+            Journals.Add(this);
+        }
+    }
+}
