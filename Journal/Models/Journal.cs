79ac3038d5769cc21bf3b477da6952321d8491f8
shakyaa89 <shashwatshakya89@gmail.com>
2025-12-20 12:09:10 +0545

    Changed UI, added tags and moods

--- Journal/Models/Journal.cs
+++ Journal/Models/Journal.cs
@@ -9,11 +9,12 @@ namespace Project.Models   // <-- Make sure this is NOT 'Journal'
         public string Content { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
 
-        // Static list to hold all journal entries
+        public List<string> Tags { get; set; } = new List<string>();
+        public string Mood { get; set; } = string.Empty;
+
         public static List<Journal> Journals { get; } = new List<Journal>();
 
-        // Method to save a journal entry
-        public void Save()
+        public void CreateJournalEntry()
         {
             Journals.Add(this);
         }
